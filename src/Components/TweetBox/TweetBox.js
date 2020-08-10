6b15a08452d480a202bd50b8be9441bc5a92239b
JHorlamide <olamidejubril68@gmail.com>
2020-08-11 00:48:18 +0100

    testing commit

--- src/Components/TweetBox/TweetBox.js
+++ src/Components/TweetBox/TweetBox.js
@@ -7,14 +7,16 @@ const TweetBox = () => {
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
-          <Avatar src="https://pbs.twimg.com/profile_images/806970003941527552/sF8UtACd_400x400.jpg" />
+          <Avatar src="https://pbs.twimg.com/profile_images/1206671022524645381/v9yd8z4T_x96.jpg" />
           <input placeholder="what's happening?" type="text" />
         </div>
+
         <input
-          placeholder="Enter image URL"
+          placeholder="Enter image url"
           type="text"
-          className="tweetBox_imageInput"
+          className="tweetBox__imageInput"
         />
+
         <Button className="tweetBox__tweetButton">Tweet</Button>
       </form>
     </div>
@@ -22,3 +24,5 @@ const TweetBox = () => {
 };
 
 export default TweetBox;
+
+// https://pbs.twimg.com/profile_images/1206671022524645381/v9yd8z4T_x96.jpg
